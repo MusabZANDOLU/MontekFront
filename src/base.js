ee93021885e693936f9c602690f76b5f5ab9f4d7
Musab ZANDOLU <100616436+MusabZANDOLU@users.noreply.github.com>
2022-11-22 18:36:20 +0300

    Update base.js

--- src/base.js
+++ src/base.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = "https://montekserver.herokuapp.com"; // host
-export const BASE_URL = "https://montek.onrender.com/"; // host
+export const BASE_URL = "https://montek.onrender.com"; // host
 // export const BASE_URL = "http://localhost:5000"; // development
