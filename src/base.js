9d2811feebf863b71ef2e2d587fa7ee781b49651
Musab ZANDOLU <100616436+MusabZANDOLU@users.noreply.github.com>
2022-12-06 14:32:07 +0300

    change BASE_URL

--- src/base.js
+++ src/base.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://montek.onrender.com"; // host
-export const BASE_URL = "http://localhost:5000"; // development
+export const BASE_URL = "https://montek.onrender.com"; // host
+// export const BASE_URL = "http://localhost:5000"; // development
